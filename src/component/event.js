652e9a844cd0235261ef430a55c63cbcad7fd38d
zxhjames <1849483422@qq.com>
2020-11-26 09:25:17 +0800

    init

---
+++ src/component/event.js
@@ -0,0 +1,85 @@
+/* global window */
+export function bind(target, name, fn) {
+  target.addEventListener(name, fn);
+}
+export function unbind(target, name, fn) {
+  target.removeEventListener(name, fn);
+}
+export function unbindClickoutside(el) {
+  if (el.xclickoutside) {
+    unbind(window.document.body, 'click', el.xclickoutside);
+    delete el.xclickoutside;
+  }
+}
+
+// the left mouse button: mousedown → mouseup → click
+// the right mouse button: mousedown → contenxtmenu → mouseup
+// the right mouse button in firefox(>65.0): mousedown → contenxtmenu → mouseup → click on window
+export function bindClickoutside(el, cb) {
+  el.xclickoutside = (evt) => {
+    // ignore double click
+    // console.log('evt:', evt);
+    if (evt.detail === 2 || el.contains(evt.target)) return;
+    if (cb) cb(el);
+    else {
+      el.hide();
+      unbindClickoutside(el);
+    }
+  };
+  bind(window.document.body, 'click', el.xclickoutside);
+}
+export function mouseMoveUp(target, movefunc, upfunc) {
+  bind(target, 'mousemove', movefunc);
+  const t = target;
+  t.xEvtUp = (evt) => {
+    // console.log('mouseup>>>');
+    unbind(target, 'mousemove', movefunc);
+    unbind(target, 'mouseup', target.xEvtUp);
+    upfunc(evt);
+  };
+  bind(target, 'mouseup', target.xEvtUp);
+}
+
+function calTouchDirection(spanx, spany, evt, cb) {
+  let direction = '';
+  // console.log('spanx:', spanx, ', spany:', spany);
+  if (Math.abs(spanx) > Math.abs(spany)) {
+    // horizontal
+    direction = spanx > 0 ? 'right' : 'left';
+    cb(direction, spanx, evt);
+  } else {
+    // vertical
+    direction = spany > 0 ? 'down' : 'up';
+    cb(direction, spany, evt);
+  }
+}
+// cb = (direction, distance) => {}
+export function bindTouch(target, { move, end }) {
+  let startx = 0;
+  let starty = 0;
+  bind(target, 'touchstart', (evt) => {
+    const { pageX, pageY } = evt.touches[0];
+    startx = pageX;
+    starty = pageY;
+  });
+  bind(target, 'touchmove', (evt) => {
+    if (!move) return;
+    const { pageX, pageY } = evt.changedTouches[0];
+    const spanx = pageX - startx;
+    const spany = pageY - starty;
+    if (Math.abs(spanx) > 10 || Math.abs(spany) > 10) {
+      // console.log('spanx:', spanx, ', spany:', spany);
+      calTouchDirection(spanx, spany, evt, move);
+      startx = pageX;
+      starty = pageY;
+    }
+    evt.preventDefault();
+  });
+  bind(target, 'touchend', (evt) => {
+    if (!end) return;
+    const { pageX, pageY } = evt.changedTouches[0];
+    const spanx = pageX - startx;
+    const spany = pageY - starty;
+    calTouchDirection(spanx, spany, evt, end);
+  });
+}
---
+++ src/index.js
@@ -0,0 +1,130 @@
+/* global window, document */
+import { h } from './component/element';
+import DataProxy from './core/data_proxy';
+import Sheet from './component/sheet';
+import Bottombar from './component/bottombar';
+import { cssPrefix } from './config';
+import { locale } from './locale/locale';
+import './index.less';
+
+
+class Spreadsheet {
+  constructor(selectors, options = {}) {
+    let targetEl = selectors;
+    this.options = options;
+    this.sheetIndex = 1;
+    this.datas = [];
+    if (typeof selectors === 'string') {
+      targetEl = document.querySelector(selectors);
+    }
+    this.bottombar = new Bottombar(() => {
+      const d = this.addSheet();
+      this.sheet.resetData(d);
+    }, (index) => {
+      const d = this.datas[index];
+      this.sheet.resetData(d);
+    }, () => {
+      this.deleteSheet();
+    }, (index, value) => {
+      this.datas[index].name = value;
+    });
+    this.data = this.addSheet();
+    const rootEl = h('div', `${cssPrefix}`)
+      .on('contextmenu', evt => evt.preventDefault());
+    // create canvas element
+    targetEl.appendChild(rootEl.el);
+    this.sheet = new Sheet(rootEl, this.data);
+    rootEl.child(this.bottombar.el);
+  }
+
+  addSheet(name, active = true) {
+    const n = name || `sheet${this.sheetIndex}`;
+    const d = new DataProxy(n, this.options);
+    d.change = (...args) => {
+      this.sheet.trigger('change', ...args);
+    };
+    this.datas.push(d);
+    // console.log('d:', n, d, this.datas);
+    this.bottombar.addItem(n, active);
+    this.sheetIndex += 1;
+    return d;
+  }
+
+  deleteSheet() {
+    const [oldIndex, nindex] = this.bottombar.deleteItem();
+    if (oldIndex >= 0) {
+      this.datas.splice(oldIndex, 1);
+      if (nindex >= 0) this.sheet.resetData(this.datas[nindex]);
+    }
+  }
+
+  loadData(data) {
+    const ds = Array.isArray(data) ? data : [data];
+    this.bottombar.clear();
+    this.datas = [];
+    if (ds.length > 0) {
+      for (let i = 0; i < ds.length; i += 1) {
+        const it = ds[i];
+        const nd = this.addSheet(it.name, i === 0);
+        nd.setData(it);
+        if (i === 0) {
+          this.sheet.resetData(nd);
+        }
+      }
+    }
+    return this;
+  }
+
+  getData() {
+    return this.datas.map(it => it.getData());
+  }
+
+  cellText(ri, ci, text, sheetIndex = 0) {
+    this.datas[sheetIndex].setCellText(ri, ci, text, 'finished');
+    return this;
+  }
+
+  cell(ri, ci, sheetIndex = 0) {
+    return this.datas[sheetIndex].getCell(ri, ci);
+  }
+
+  cellStyle(ri, ci, sheetIndex = 0) {
+    return this.datas[sheetIndex].getCellStyle(ri, ci);
+  }
+
+  reRender() {
+    this.sheet.table.render();
+    return this;
+  }
+
+  on(eventName, func) {
+    this.sheet.on(eventName, func);
+    return this;
+  }
+
+  validate() {
+    const { validations } = this.data;
+    return validations.errors.size <= 0;
+  }
+
+  change(cb) {
+    this.sheet.on('change', cb);
+    return this;
+  }
+
+  static locale(lang, message) {
+    locale(lang, message);
+  }
+}
+
+const spreadsheet = (el, options = {}) => new Spreadsheet(el, options);
+
+if (window) {
+  window.x_spreadsheet = spreadsheet;
+  window.x_spreadsheet.locale = (lang, message) => locale(lang, message);
+}
+
+export default Spreadsheet;
+export {
+  spreadsheet,
+};
